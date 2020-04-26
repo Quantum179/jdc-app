fb2886ed5916aa890e9f44a47c3dca622d65dbf4
Quantum179 <steeven.tell@gmail.com>
2020-04-25 22:41:00 -0400

    starting mobile first design

--- web/src/App.vue
+++ web/src/App.vue
@@ -5,106 +5,102 @@
 </template>
 
 <style lang="stylus">
+@import "./style/app"
+
 body
   margin 0
   background-color #F5DEB3
   color black
 
-.navbar
-  position fixed
-  display flex
-  justify-content center
-  width 100%
-  height 100px
+// .navbar
+//   position fixed
+//   display flex
+//   justify-content center
+//   width 100%
+//   height 100px
 
-.tab-list
-  display flex
-  justify-content space-between
-  align-items center
-  width 60%
-  background-color white
-  margin 0 15px
-  & > span
-    font-size 2em
-    &:hover, &:active
-      text-decoration underline
+// .tab-list
+//   display flex
+//   justify-content space-between
+//   align-items center
+//   width 60%
+//   background-color white
+//   margin 0 15px
+//   & > span
+//     font-size 2em
+//     &:hover, &:active
+//       text-decoration underline
 
-.flex-center
-  display flex
-  justify-content center
-  align-items center
+// .section
+//   @extend .flex-center
+//   width 100%
+//   height 100vh
 
-.section
-  @extend .flex-center
-  width 100%
-  height 100vh
-  overflow-y hidden
+// .btn-list
+//   grid-area btn
+//   width 100%
+//   height 100%
+//   display flex
+//   justify-content space-between
+//   align-items center
 
-.btn-list
-  grid-area btn
-  width 100%
-  height 100%
-  display flex
-  justify-content space-between
-  align-items center
+// .btn {
+//   @extend .flex-center
+//   width 30%
+//   height 80%
+//   font-size 2em
+//   cursor pointer
+// }
 
-.btn {
-  @extend .flex-center
-  width 30%
-  height 80%
-  font-size 2em
-  cursor pointer
-}
+// .btn:hover {
+//   background-color rgba(255, 255, 255, 0.6)
+//   border 2px solid white
+//   border-radius 5px
+//   text-decoration underline
+// }
 
-.btn:hover {
-  background-color rgba(255, 255, 255, 0.6)
-  border 2px solid white
-  border-radius 5px
-  text-decoration underline
-}
+// h1, h2 {
+//   grid-area title
+// }
 
-h1, h2 {
-  grid-area title
-}
+// h1 {
+//   font-size 7em
+//   text-decoration underline
+// }
 
-h1 {
-  font-size 7em
-  text-decoration underline
-}
+// h2 {
+//   font-size 5em
+//   text-decoration underline
+// }
 
-h2 {
-  font-size 5em
-  text-decoration underline
-}
+// .article-list
+//   grid-area list
+//   display flex
+//   flex-wrap wrap
+//   // display: grid;
+//   // grid-auto-flow: column;
+//   // grid-auto-rows: auto;
+//   // grid-template-columns: repeat(5, 1fr);
+//   width 100%
+//   min-height 50%
 
-.article-list
-  grid-area list
-  display flex
-  flex-wrap wrap
-  // display: grid;
-  // grid-auto-flow: column;
-  // grid-auto-rows: auto;
-  // grid-template-columns: repeat(5, 1fr);
-  width 100%
-  min-height 50%
+// .article-card
+//   @extend .flex-center
+//   width 200px
+//   height 200px
+//   margin 10px
+//   cursor pointer
 
-.article-card
-  @extend .flex-center
-  width 200px
-  height 200px
-  margin 10px
-  cursor pointer
+// .article-btn
+//   grid-area btn
 
-.article-btn
-  grid-area btn
+// .link-list
+//   grid-area link
+//   display flex
+//   justify-content space-between
+//   align-items center
+//   width 100%
 
-.link-list
-  grid-area link
-  display flex
-  justify-content space-between
-  align-items center
-  width 100%
-
-.link
-  border-radius 50%
+// .link
+//   border-radius 50%
 </style>
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="home">
     <div class="navbar">
-      <p style="color:red">Navbar temporaire</p>
+      <!-- <p style="color:red">temporaire</p> -->
       <div class="tab-list">
           <span @click="section = 1">Présentation</span>
           <span @click="section = 2">Articles</span>
           <span @click="section = 3">A propos</span>
       </div>
-      <p style="color:red">Navbar temporaire</p>
+      <!-- <p style="color:red">temporaire</p> -->
     </div>
     <div class="section">
       <transition name="fade" mode="out-in">
@@ -20,11 +20,11 @@
           </div>
         </div>
         <div class="intro" key="2" v-if="section === 1">
-          <h2>Présentation</h2>
-          <p>Le Journal du Confiné est un blog que j'ai décidé de lancer pour stimuler ma productivité et partager mes activités de confinement.<br><br>Le développement du blog est le premier des nombreux défis que j'aimerais relever. Je me permets donc de le mettre volontairement en ligne dès le début de la phase de développement, pendant qu'il est encore inachevé.<br><br>Cela permettra ainsi de voir son évolution au fil des mises à jour du code, donc ne soyez pas trop sévères si certaines fonctionnalités sont incomplètes ou certains visuels moches.<br>Il y aura bientôt un espace commentaire, pour ceux qui souhaitent participer au développement en faisant des retours utilisateurs.<br><br>A bientôt pour les premiers articles ! </p>
+          <h1>Présentation</h1>
+          <p>Le Journal du Confiné est un blog que j'ai décidé de lancer pour stimuler ma productivité et partager mes activités de confinement.<br><br>Le développement du blog est le premier des nombreux défis que j'aimerais relever. Je me permets donc de le mettre volontairement en ligne dès le début de la phase de développement, pendant qu'il est encore inachevé.<br><br>Cela permettra ainsi de voir son évolution au fil des mises à jour du code, donc ne soyez pas trop sévères si certaines fonctionnalités sont incomplètes ou certains visuels moches.<br>Il y aura bientôt un espace commentaire, pour ceux qui souhaitent participer au développement en faisant des retours utilisateurs.<br><br>A bientôt pour les premiers articles !<br></p>
         </div>
         <div class="article" key="3" v-if="section === 2">
-          <h2>Articles</h2>
+          <h1>Articles</h1>
           <div class="article-list">
             <div class="article-card">
               <h3>Lancement du blog</h3>
@@ -38,17 +38,17 @@
             <div class="article-card">
               <h3>Article de test 3</h3>
             </div>
-            <div class="article-card">
+            <div class="article-card" v-if="mediaSize === 'desktop'">
               <h3>Article de test 4</h3>
             </div>
-            <div class="article-card">
+            <div class="article-card" v-if="mediaSize === 'desktop'">
               <h3>Article de test 5</h3>
             </div>
           </div>
           <div class="btn article-btn">Voir tout</div>
         </div>
         <div class="about" key="4" v-if="section === 3">
-          <h2>A propos</h2>
+          <h1>A propos</h1>
           <p>sdipfjezipf ipsefsipdfblzefsfo ezipfjsdf eipfi sd spidfjp sfipdfjs spfjspdif spifjsipf pfsjpisdf sfpuqpfs psdfjpsifjp psdfjsipfj sdipfjezipf ipsefsipdfblzefsfo ezipfjsdf eipfi sd spidfjp sfipdfjs spfjspdif spifjsipf pfsjpisdf sfpuqpfs psdfjpsifjp psdfjsipfj sdipfjezipf ipsefsipdfblzefsfo ezipfjsdf eipfi sd spidfjp sfipdfjs spfjspdif spifjsipf pfsjpisdf sfpuqpfs psdfjpsifjp psdfjsipfj sdipfjezipf ipsefsipdfblzefsfo ezipfjsdf eipfi sd spidfjp sfipdfjs spfjspdif spifjsipf pfsjpisdf sfpuqpfs psdfjpsifjp psdfjsipfj sdipfjezipf ipsefsipdfblzefsfo ezipfjsdf eipfi sd spidfjp sfipdfjs spfjspdif spifjsipf pfsjpisdf sfpuqpfs psdfjpsifjp psdfjsipfj sdipfjezipf ipsefsipdfblzefsfo ezipfjsdf eipfi sd spidfjp sfipdfjs spfjspdif spifjsipf pfsjpisdf sfpuqpfs psdfjpsifjp psdfjsipfj sdipfjezipf ipsefsipdfblzefsfo ezipfjsdf eipfi sd spidfjp sfipdfjs spfjspdif spifjsipf pfsjpisdf sfpuqpfs psdfjpsifjp psdfjsipfj sdipfjezipf ipsefsipdf</p>
           <div class="link-list">
             <div class="link">Twitter</div>
@@ -67,50 +67,80 @@ export default {
   name: 'Home',
   data() {
     return {
-      section: 0
+      section: 0,
+      mediaSize: 'mobile',
+      mq: null
     }
   },
-  methods: {
-    // changeSection() {
-    //   if (this.scrolled) { return }
-
-    //   console.log(window.scrollY)
-    //   console.log(this.scrollY)
-
-    //   if (window.scrollY > this.scrollY && this.section < 3) {
-    //     this.scrolled = true
-    //     this.section += 1
-    //   } else if (window.scrollY < this.scrollY && this.section > 0) {
-    //     this.scrolled = true
-    //     this.section -= 1
-    //   }
-
-    //   setTimeout(() => {
-    //     window.scrollTo(0, 5)
-    //     this.scrollY = 5
-    //     this.scrolled = false
-    //   }, 2000)
-    // }
+  created() {
+    if (matchMedia) {
+      this.mq = window.matchMedia('(min-width:768px)')
+      this.mq.addListener(this.changeMediaSize)
+      this.changeMediaSize(this.mq)
+    }
+    // window.addEventListener('scroll', this.changeSection);
   },
-  // created() {
-  //   window.addEventListener('scroll', this.changeSection);
-  // },
-  // destroyed() {
-  //   window.removeEventListener('scroll', this.changeSection);
-  // }
+  destroyed() {
+    if (matchMedia) {
+      this.mq.removeListener(this.changeMediaSize)
+    }
+    // window.removeEventListener('scroll', this.changeSection);
+  },
+  methods: {
+    changeMediaSize(mq) {
+      console.log('test')
+      if (mq.matches) {
+        this.mediaSize = 'desktop'
+      } else {
+        this.mediaSize = 'mobile'
+      }
+    },
+    changeSection() {
+      if (this.scrolled) { return }
+
+      console.log(window.scrollY)
+      console.log(this.scrollY)
+
+      if (window.scrollY > this.scrollY && this.section < 3) {
+        this.scrolled = true
+        this.section += 1
+      } else if (window.scrollY < this.scrollY && this.section > 0) {
+        this.scrolled = true
+        this.section -= 1
+      }
+
+      setTimeout(() => {
+        window.scrollTo(0, 5)
+        this.scrollY = 5
+        this.scrolled = false
+      }, 2000)
+    }
+  },
+
 }
 </script>
 
 <style lang="stylus">
-.container
-  display grid
-  justify-items center
-  width 90%
-  height 90%
+@import "../style/app"
 
-p
-  grid-area text
-  font-size 2em
+.navbar
+  position fixed
+  display flex
+  justify-content center
+  width 100%
+  height 50px
+
+.tab-list
+  display flex
+  justify-content space-between
+  align-items center
+  width 80%
+  background-color white
+  margin 0 10px
+  padding 0 10px
+  & > span:hover
+    font-size 1.2em
+    text-decoration underline
 
 .default, .intro, .article, .about
   @extend .container
@@ -120,11 +150,11 @@ p
   grid-template-areas:
   '. . .'\
   'title title title'\
-  'btn btn btn'\
+  '. btn .'\
   '. . .'
-  grid-template-rows 0.20fr 2.5fr 1fr 0.5fr
-  grid-template-columns repeat(3, 1fr)
-  grid-gap 20px
+  grid-template-rows 0.25fr 1.5fr 3fr 0.25fr
+  grid-template-columns 1fr 2fr 1fr
+  grid-gap 10px
 
 .intro
   grid-template-areas:
@@ -132,7 +162,7 @@ p
   'title title title'\
   'text text text'\
   '. . .'
-  grid-template-rows 0.20fr 1fr 2.5fr 0.5fr
+  grid-template-rows 0.25fr 1fr 2fr 0.25fr
   grid-template-columns repeat(3, 1fr)
 
 .article
@@ -142,7 +172,7 @@ p
   'list list list'\
   '. btn .'\
   '. . .'
-  grid-template-rows 0.20fr 1fr 3fr 1fr 0.5fr
+  grid-template-rows 0.25fr 1fr auto 1fr 0.25fr
   grid-template-columns repeat(3, 1fr)
 
 .about
@@ -152,6 +182,102 @@ p
   'text text text'\
   'link link link'\
   '. . .'
+
+.btn-list
+  grid-area btn
+  display flex
+  flex-direction column
+  justify-content center
+  align-items space-between
+  width 100%
+  height 100%
+
+.btn
+  @extend .flex-center
+  width 100%
+  height 80%
+  font-size 2em
+  cursor pointer
+
+.btn:hover
+  background-color rgba(255, 255, 255, 0.6)
+  border 2px solid white
+  border-radius 5px
+  text-decoration underline
+
+.article-list
+  grid-area list
+  display flex
+  justify-content center
+  flex-wrap wrap
+  // display: grid;
+  // grid-auto-flow: column;
+  // grid-auto-rows: auto;
+  // grid-template-columns: repeat(5, 1fr);
+  width 100%
+  min-height 50%
+
+.article-card
+  @extend .flex-center
+  width 120px
+  height 120px
+  margin 10px
+  cursor pointer
+  background-color rgba(255, 255, 255, 0.7)
+
+.article-btn
+  grid-area btn
+
+// .link-list
+//   grid-area link
+//   display flex
+//   justify-content space-between
+//   align-items center
+//   width 100%
+
+// .link
+//   border-radius 50%
+
+// .default, .intro, .article, .about
+//   @extend .container
+
+// .default
+//   align-items center
+//   grid-template-areas:
+//   '. . .'\
+//   'title title title'\
+//   'btn btn btn'\
+//   '. . .'
+//   grid-template-rows 0.20fr 2.5fr 1fr 0.5fr
+//   grid-template-columns repeat(3, 1fr)
+//   grid-gap 20px
+
+// .intro
+//   grid-template-areas:
+//   '. . .'\
+//   'title title title'\
+//   'text text text'\
+//   '. . .'
+//   grid-template-rows 0.20fr 1fr 2.5fr 0.5fr
+//   grid-template-columns repeat(3, 1fr)
+
+// .article
+//   grid-template-areas:
+//   '. . .'\
+//   'title title title'\
+//   'list list list'\
+//   '. btn .'\
+//   '. . .'
+//   grid-template-rows 0.20fr 1fr 3fr 1fr 0.5fr
+//   grid-template-columns repeat(3, 1fr)
+
+// .about
+//   grid-template-areas:
+//   '. . .'\
+//   'title title title'\
+//   'text text text'\
+//   'link link link'\
+//   '. . .'
 
 .fade-enter-active, .fade-leave-active
   transition opacity 1s
